0e92d0db6b808df8075befb10050bc49c8363fc5
Kishorgujar <91127600+Kishorgujar@users.noreply.github.com>
2024-10-17 03:09:39 +0530

    Update outputs.tf

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -12,4 +12,3 @@ output "cluster_certificate_authority" {
   description = "The certificate authority data for the EKS cluster."
   value       = aws_eks_cluster.this.certificate_authority[0].data
 }
-
