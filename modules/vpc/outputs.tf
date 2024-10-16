eb1e4a7bb6ba0c0dae11d0ee32cbfbdb02f68854
Kishorgujar <91127600+Kishorgujar@users.noreply.github.com>
2024-10-17 03:17:19 +0530

    Update outputs.tf

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,15 +1,11 @@
-output "public_subnet1_ids" {
-  description = "List of public subnet IDs."
-  value       = aws_subnet.public_pub_sub1[*].id
+output "public_pub_subnet1_ids" {
+  value = aws_subnet.public_pub_sub1[*].id  # Keep this if there are multiple subnets
 }
 
-output "public_subnet2_ids" {
-  description = "List of public subnet 2 IDs."
-  value       = aws_subnet.public_pub_sub2[*].id
+output "public_pub_subnet2_ids" {
+  value = aws_subnet.public_pub_sub2[*].id  # Keep this if there are multiple subnets
 }
 
 output "vpc_id" {
-  description = "The ID of the VPC."
-  value       = aws_vpc.public_vpc.id
+  value = aws_vpc.public_vpc.id
 }
-
